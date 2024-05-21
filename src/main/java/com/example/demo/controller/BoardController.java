5477a0d32f2a80c58ada4c8c23fa4d5bd4a8acba
tjdan805 <tjdan805@gmail.com>
2024-05-21 09:12:32 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.domain.request.BoardRequest;
+import com.example.demo.domain.response.BoardResponse;
+import com.example.demo.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/boards")
+@RequiredArgsConstructor
+@CrossOrigin(
+        allowedHeaders = "*",
+        origins = "*",
+        methods = {RequestMethod.GET,RequestMethod.POST,RequestMethod.PUT,RequestMethod.DELETE, RequestMethod.OPTIONS }
+)
+public class BoardController {
+    private final BoardService boardService;
+    @GetMapping
+    public List<BoardResponse> getAllBoard(){
+        return boardService.getAll();
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void saveBoard(@RequestBody BoardRequest request){
+        boardService.saveBoard(request);
+    }
+}
---
+++ src/main/java/com/example/demo/service/BoardServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.entity.Board;
+import com.example.demo.domain.request.BoardRequest;
+import com.example.demo.domain.response.BoardResponse;
+import com.example.demo.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService {
+    private final BoardRepository boardRepository;
+
+    @Override
+    public BoardResponse getById(Long id) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(IllegalArgumentException::new);
+        return BoardResponse.of(board);
+    }
+
+    @Override
+    public List<BoardResponse> getAll() {
+        return boardRepository.findAll()
+                .stream()
+                .map(BoardResponse::of)
+                .toList();
+    }
+
+    @Override
+    public void saveBoard(BoardRequest request) {
+        boardRepository.save(request.toEntity());
+    }
+}
