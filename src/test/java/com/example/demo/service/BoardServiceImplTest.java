306b1ffebff840278b478f8d00ad4be049b16851
tjdan805 <tjdan805@gmail.com>
2024-05-21 09:34:17 +0900

    test

--- src/test/java/com/example/demo/service/BoardServiceImplTest.java
+++ src/test/java/com/example/demo/service/BoardServiceImplTest.java
@@ -11,7 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
+//
 import java.util.List;
 import java.util.Optional;
 
